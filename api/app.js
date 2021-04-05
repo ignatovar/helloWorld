1224edea45ca40d7b9097131db0760cae06658cd
ignatovar <ignatovar@gmail.com>
2021-04-05 13:27:59 -0500

    Added CORS to project

--- api/app.js
+++ api/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var comicsRouter = require('./routes/comics');
 
+var cors = require('cors')
 var app = express();
 
 // view engine setup
@@ -20,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/comics', comicsRouter);
